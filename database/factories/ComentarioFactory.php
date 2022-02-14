0f7fdc9c4202482912aa5c89a8ae0fcb8fabfaee
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2022-02-14 19:46:29 +0100

    Migración y seeder de Comentario

---
+++ database/factories/ComentarioFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comentario>
+ */
+class ComentarioFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'email' => $this->faker->email(),
+            'texto' => $this->faker->text(100),
+            'fecha' => $this->faker->dateTime(),
+            'visible' => $this->faker->boolean(),
+        ];
+    }
+}
